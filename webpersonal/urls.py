cbf2fe5e5d301623e7dd6af23529902300a43961
Carlos <jcmurblan@gmail.com>
2019-10-27 00:57:23 +0200

    creada la app core y plantillas con base

--- webpersonal/urls.py
+++ webpersonal/urls.py
@@ -18,6 +18,10 @@ from django.urls import path
 from core import views
 
 urlpatterns = [
+    path("", views.home, name="home"),
+    path('about-me/', views.about, name="about"),
+    path('portfolio/', views.portfolio, name="portfolio"),
+    path('contact/', views.contact, name="contacto"),
     path('admin/', admin.site.urls),
-    path("", views.home, name='home'),
+
 ]
